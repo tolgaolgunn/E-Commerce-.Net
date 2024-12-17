dc154cd1133d2fe1ab62ed935bd12fc057cae00c
tolgaolgun42 <121184586+tolgaolgun42@users.noreply.github.com>
2024-12-17 18:44:35 +0300

    product detail page added

---
+++ MVCOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MVCOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        // GET: UrunDetay
+        Context c=new Context();
+        public ActionResult Index()
+        {
+            Class1 cs=new Class1();
+            //var degerler=c.Uruns.Where(x=>x.UrunID==1).ToList();
+            cs.Urun = c.Uruns.Where(x => x.UrunID == 1).ToList();
+            cs.Detay=c.Detays.Where(y=>y.DetayID==1).ToList();
+            return View(cs);
+        }
+    }
+}
---
+++ MVCOnlineTicariOtomasyon/Models/Siniflar/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Class1
+    {
+        public IEnumerable<Urun> Urun { get; set; }
+        public IEnumerable<Detay> Detay { get; set; }
+    }
+}
